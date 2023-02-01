c9137635d9fefb0f76d5b420caeb0b3d6ba6d68e
vinod102003 <123479516+vinod102003@users.noreply.github.com>
2023-02-01 08:18:21 +0530

    Add files via upload

---
+++ arithematicsoperations.java
@@ -0,0 +1,13 @@
+
+public class arithematicsoperations {
+    public static void main(String[] args) {
+        int Q  = 10;
+        int P = 12;
+        System.out.println(P+Q);
+        System.out.println(P/Q);
+        System.out.println(P*Q);
+        System.out.println(P-Q);
+        System.out.println(P%Q);
+        System.out.println(args[0]);
+    }
+}
